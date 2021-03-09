3154e77026a673e19a93c514598a957055c93f7f
belayakaryna <belaya.karina1998@gmail.com>
2021-03-09 21:47:05 +0300

    feat: add code in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length == 0 || array.length == 0){
+    return 0;
+  }
+  else{
+    mi=array[0]
+    for (i=0; i<array.length;i++){
+     if(mi>array[i]){
+      mi=array[i]
+     }
+    }
+    return(mi)
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length == 0 || array.length == 0){
+    return 0;
+  }
+  else{
+    ma=array[0]
+    for (i=0; i<array.length;i++){
+     if(ma<array[i]){
+      ma=array[i]
+     }
+    }
+    return(ma)
+  }
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length == 0 || array.length == 0){
+    return 0;
+  }
+  else{
+    sum=0;
+    for (i=0; i<array.length;i++){
+     sum+=array[i]
+    }
+    return(sum/array.length)
+  }
 }
+
